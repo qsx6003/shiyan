afa5c36cbec6b78b694a03724061620d17c4c31a
qsx <qsx6003@qq.com>
2018-11-16 16:23:47 +0800

    机器人聊天

--- itchat/tulin-post.py
+++ itchat/tulin-post.py
@@ -7,17 +7,21 @@ def get_response(msg):
         'info'   : msg,
         'userid' : '空空',
     }
+    
     try:
         r = requests.post(apiUrl, data=data).json()
         return r.get('text')
     except:
         return
+
 def chat(a):
-    if a == 1:
-        print("TL: ",get_response("我来啦"))
-    else:
-        tt = get_response(input("MY: "))        
-        print("TL: ",tt)
-    return tt
+    while True:
+        if a == 0:
+            print("TL: ",get_response("我来啦"))
+        else:
+            tt = get_response(input("MY: "))        
+            print("TL: ",tt)
+        a = 1
 if __name__ == "__main__":
+    chat(0)
 
